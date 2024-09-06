6acaefb7e84c7882f0e6e807ae9eeb361207570f
Rddgrns <loppy.rv@yandex.ru>
2024-09-06 16:06:37 +0300

    Initial commit

---
+++ src/main/java/com/example/RapsBotBetaZOV/RapsBotBetaZovApplication.java
@@ -0,0 +1,13 @@
+package com.example.RapsBotBetaZOV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapsBotBetaZovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapsBotBetaZovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RapsBotBetaZOV/RapsBotBetaZovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RapsBotBetaZOV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapsBotBetaZovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
